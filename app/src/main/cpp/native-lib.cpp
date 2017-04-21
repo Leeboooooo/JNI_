4488ced8a9ddda0c533bf9e579fa5ce112da4429
libo <Libo@mobilekm.cn>
2017-04-21 16:18:32 +0800

    如果需要生成的.so给其它项目提供支持，需要把c/c++函数的包名和项目一致。参考源码中的 Java_com_mobao_utils_iupgrade_PatchUtils_patch Java_后面表示项目中native类的包名

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ JNIEXPORT jstring JNICALL
 Java_com_example_mobaolibo_jni_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Just new version";
+    std::string hello = "Just old version";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/example/mobaolibo/jni/bsdiff/PatchUtils.java
+++ app/src/main/java/com/example/mobaolibo/jni/bsdiff/PatchUtils.java
@@ -21,21 +21,6 @@ public class PatchUtils {
 	}
 
 	/**
-	 * native方法 比较路径为oldPath的apk与newPath的apk之间差异，并生成patch包，存储于patchPath
-	 * 
-	 * 返回：0，说明操作成功
-	 * 
-	 * @param oldApkPath
-	 *            示例:/sdcard/old.apk
-	 * @param newApkPath
-	 *            示例:/sdcard/new.apk
-	 * @param patchPath
-	 *            示例:/sdcard/xx.patch
-	 * @return
-	 */
-	public native int genDiff(String oldApkPath, String newApkPath, String patchPath);
-
-	/**
 	 * native方法 使用路径为oldApkPath的apk与路径为patchPath的补丁包，合成新的apk，并存储于newApkPath
 	 *
 	 * 返回：0，说明操作成功
